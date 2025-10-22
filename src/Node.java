365c6d4860ff971b613375207d03f8d751d5b477
AnaSaless <166721815+AnaSaless@users.noreply.github.com>
2025-10-22 19:36:25 -0300

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,10 +1,15 @@
 
 public class Node {
+    // Guarda a informação principal
     private Elemento informacao;
+    // Aponta para o próximo nó da lista/fila/pilha.
     private Node proximo;
 
+    // Construtor: é chamado quando um novo nó é criado.
     public Node(Elemento informacao) {
+        // Guarda a informação (Elemento) que foi passada.
         this.informacao = informacao;
+        // O próximo nó começa como nulo.
         this.proximo = null;
     }
 
