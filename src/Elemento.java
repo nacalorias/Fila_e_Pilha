5450a38e88ac64de70432e94293984292f03a46a
AnaSaless <166721815+AnaSaless@users.noreply.github.com>
2025-10-22 19:25:12 -0300

    Update Elemento.java

--- src/Elemento.java
+++ src/Elemento.java
@@ -1,9 +1,13 @@
-
+// Ela serve apenas para guardar três informações juntas.
 public class Elemento {
+    // Variáveis para guardar os dados (strings).
     String campo1;
     String campo2;
     String campo3;
 
+
+    // Construtor: é chamado quando um novo 'Elemento' é criado.
+    // Ele recebe os três valores e os armazena nas variáveis da classe.
     public Elemento(String c1, String c2, String c3) {
         this.campo1 = c1;
         this.campo2 = c2;
